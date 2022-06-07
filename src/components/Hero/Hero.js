5b221465a9b58197304871c63b8092301f7c376b
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-06-08 00:43:52 +0600

    hero section gsap animation sample added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Hero.css";
 import HeroCard from "./HeroCard";
+import { gsap } from "gsap";
 
 const Hero = () => {
+  let heroSection = useRef(null);
+
+  useEffect(() => {
+    console.log(heroSection);
+    gsap.to(heroSection.current, {
+      duration: 1,
+      css: { visibility: "visible", opacity: 1 },
+    });
+  }, []);
+
   return (
-    <section className="py-10">
+    <section ref={heroSection} className="py-10 hero-section">
       <div className="container mx-auto hero-title">
         <h1 className="text-3xl md:text-5xl text-left md:text-center leading-tight mt-10">
           Ridwan Suhel <br className="hidden lg:block" />
