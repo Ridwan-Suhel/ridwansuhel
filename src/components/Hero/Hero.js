2c0472aafc246a0efa03e6c06aa337325e5eb7b5
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-06-08 02:35:50 +0600

    sample stagger added for practice

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,27 +1,71 @@
 import React, { useEffect, useRef } from "react";
 import "./Hero.css";
 import HeroCard from "./HeroCard";
-import { gsap } from "gsap";
+import { gsap, Power2 } from "gsap";
 
 const Hero = () => {
+  let tl = useRef();
   let heroSection = useRef(null);
+  let heroTitle = useRef(null);
+  let heroTitle2 = useRef(null);
+  let text = useRef(null);
 
   useEffect(() => {
-    console.log(heroSection);
     gsap.to(heroSection.current, {
       duration: 1,
       css: { visibility: "visible", opacity: 1 },
     });
+
+    // gsap.to(heroTitle.current, {
+    //   duration: 1,
+    //   y: 0,
+    // });
+    // gsap.to(heroTitle2.current, {
+    //   duration: 1,
+    //   y: 0,
+    // });
+
+    tl.current = gsap.timeline();
+
+    tl.current.to(heroTitle.current, { duration: 0.7, y: 0 });
+
+    tl.current.to(".text", { y: 0, stagger: 0.2 }, "<");
   }, []);
 
   return (
     <section ref={heroSection} className="py-10 hero-section">
       <div className="container mx-auto hero-title">
-        <h1 className="text-3xl md:text-5xl text-left md:text-center leading-tight mt-10">
+        <h1
+          ref={heroTitle}
+          className="heroTitle text-3xl md:text-5xl text-left md:text-center leading-tight mt-10"
+        >
           Ridwan Suhel <br className="hidden lg:block" />
           Frond-End Web Developer <br className="hidden lg:block" />
           Based In Bangladesh
         </h1>
+        {/* <h1
+          ref={heroTitle2}
+          className="heroTitle text-3xl md:text-5xl text-left md:text-center leading-tight mt-10"
+        >
+          Ridwan Suhel <br className="hidden lg:block" />
+          Frond-End Web Developer <br className="hidden lg:block" />
+          Based In Bangladesh
+        </h1> */}
+
+        <ul className="mt-16 text-lg text-center">
+          <li className="text" ref={text}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing.
+          </li>
+          <li className="text" ref={text}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing.
+          </li>
+          <li className="text" ref={text}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing.
+          </li>
+          <li className="text" ref={text}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing.
+          </li>
+        </ul>
 
         <div className="card-wrapper mt-16">
           <HeroCard />
